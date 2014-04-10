858074c6f26c4539cc9646dcbdb09785e39c150d
FredericoButzke <butzkefrederico@gmail.com>
2014-04-10 14:19:24 -0600

    grid updated

--- Grid.py
+++ Grid.py
@@ -2,13 +2,13 @@ class Grid():
 	"This class implements a hash table that"
 	"is a Grid."
 	def __init__(self):
-		self.grid = list()
+		self.grid = dict()
 		
 	def getGrid(self):
 		return self.grid
 
 	def addCell(self, cell, x, y):
-		self.grid.insert(cell, [x, y])
+		self.grid[cell] = [x, y]
 
 	def updateCell(self, cell, x, y):
 		self.grid[cell] = [x, y]
--- Simulated_Annealing.py
+++ Simulated_Annealing.py
@@ -1,4 +1,5 @@
 from FileRead import FileRead
+from Grid import Grid
 
 fl = FileRead("hw3.data", "r")
 gridx, gridy, nets, fixed_cells, cells = fl.getData() 
@@ -8,3 +9,4 @@ print "y: ", gridy
 print "nets: \n", [net for net in nets.getNets()]
 print "cells: \n", cells
 print "fixed cells: \n", fixed_cells
+
